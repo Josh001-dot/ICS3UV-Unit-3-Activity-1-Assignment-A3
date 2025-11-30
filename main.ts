215a606287a5932ffe543eff116cb85131a300d4
default <joshua.adeyemi@stu.ocsb.ca>
2025-11-30 06:50:09 +0000

    commit message

---
+++ main.ts
@@ -0,0 +1,28 @@
+/**
+ * @author Joshua Adeyemi
+ * @version 1.0.0
+ * @date 2025-11-30
+ * @fileoverview This program displays the multiplication table of 9 from 0 to 12 without using loops.
+ */
+
+// initialize number
+const NUMBER: number = 9;
+
+// INPUT - none
+
+// PROCESS & OUTPUT
+console.log("0 × 9 = " + (0 * NUMBER));
+console.log("1 × 9 = " + (1 * NUMBER));
+console.log("2 × 9 = " + (2 * NUMBER));
+console.log("3 × 9 = " + (3 * NUMBER));
+console.log("4 × 9 = " + (4 * NUMBER));
+console.log("5 × 9 = " + (5 * NUMBER));
+console.log("6 × 9 = " + (6 * NUMBER));
+console.log("7 × 9 = " + (7 * NUMBER));
+console.log("8 × 9 = " + (8 * NUMBER));
+console.log("9 × 9 = " + (9 * NUMBER));
+console.log("10 × 9 = " + (10 * NUMBER));
+console.log("11 × 9 = " + (11 * NUMBER));
+console.log("12 × 9 = " + (12 * NUMBER));
+
+console.log("\nDone.");
